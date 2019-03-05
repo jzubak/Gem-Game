877ca0a994cb5695cee41e26f3b5a43c427827f1
jzubak <jzubak81@gmail.com>
2019-03-04 22:07:03 -0500

    MONDAY 1006

--- assets/game.js/game.js
+++ assets/game.js/game.js
@@ -16,36 +16,38 @@ for (var i = 0; i < gemsCollected.length; i++) {
 }
 
 
-// function play() {
-    $("#start").on("click", function () {
-        targetnumber = Math.floor(19 + Math.random() * (120 + 1 - 19));
-        $("#target").text(targetnumber);
-        counter = 0;
-    });
+$("#start").on("click", function () {
+    targetnumber = Math.floor(19 + Math.random() * (120 + 1 - 19));
+    $("#target").text(targetnumber);
+    counter = 0;
+    console.log(targetnumber)
+});
 
 
 $(".gem-image").on("click", function () {
     var gemsCollected = Math.floor(Math.random() * (12 - 1) + 1);
     gemsCollected = parseInt(gemsCollected);
     counter += gemsCollected;
-    $("#Collected").text(counter)
-    console.log(gemsCollected);
+    $("#Collected").text(counter);
+    
 
     if (counter === targetnumber) {
-        alert("Winner Winner Chiceken Dinner!");
+        alert("Winner Winner Chicken Dinner!");
         wins++
         $("#win").text(wins);
-        // play()
-        }
+        alert("Click 'Start Searching' to play again");
+    }
 
     else if (counter >= targetnumber) {
         alert("Womp womp, you're a loser!!");
         loss++;
         $("#losses").text(loss);
-        // play();
+        alert("Click 'Start Searching' to play again");
     }
+
+
 });
-// }
+
 
 
 
